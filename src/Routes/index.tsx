37232f5344880fc3f11491e8fec6471484d555eb
Jhon coneo <jconeo117@gmail.com>
2025-09-30 00:18:18 -0500

    modify index routes

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,27 +1,51 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import HomePage from '../Pages/HomePage.tsx';
-import LoginPage from '../Pages/LoginPage.tsx';
-import RegisterPage from '../Pages/RegisterPage.tsx';
-import App from '../App.tsx';
+import MainLayout from '../Components/MainLayout';
+import LoginPage from '../Pages/LoginPage';
+import RegisterPage from '../Pages/RegisterPage';
+import Dashboard from '../Pages/Dashboard';
+import CharacterPage from '../Pages/CharacterPage';
 
 const router = createBrowserRouter([
   {
+    path: '/login',
+    element: <LoginPage />,
+  },
+  {
+    path: '/register',
+    element: <RegisterPage />,
+  },
+  {
     path: '/',
-    element: <App />, // App será el layout principal
+    element: <MainLayout />,
     children: [
       {
-        index: true, // Esta es la ruta por defecto ('/')
-        element: <HomePage />,
+        index: true,
+        element: <Dashboard />,
       },
       {
-        path: 'login',
-        element: <LoginPage />,
+        path: 'dashboard',
+        element: <Dashboard />,
       },
       {
-        path: 'register',
-        element: <RegisterPage />,
+        path: 'character',
+        element: <CharacterPage/>,
       },
-      // Aquí añadiremos más rutas después (como /auctions)
+      {
+        path: 'inventory',
+        element: <div>Inventory Page</div>,
+      },
+      {
+        path: 'shop',
+        element: <div>Shop Page</div>,
+      },
+      {
+        path: 'auctions',
+        element: <div>Auctions Page</div>,
+      },
+      {
+        path: 'dungeons',
+        element: <div>Dungeons Page</div>,
+      },
     ],
   },
 ]);
